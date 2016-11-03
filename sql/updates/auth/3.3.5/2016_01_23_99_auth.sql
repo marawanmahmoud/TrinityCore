f5f273d9763d15b6d788aa8144c7a05b59b73946
ariel- <ariel-@users.noreply.github.com>
2016-11-02 23:17:23 -0300

    Merge command rbacs, link missing ones

--- sql/updates/auth/3.3.5/2016_01_23_99_auth.sql
+++ sql/updates/auth/3.3.5/2016_01_23_99_auth.sql
@@ -1,12 +1,13 @@
-DELETE FROM `rbac_permissions` WHERE `id` BETWEEN 842 AND 846;
+DELETE FROM `rbac_permissions` WHERE `id` BETWEEN 845 AND 850;
 INSERT INTO `rbac_permissions` (`id`, `name`) VALUES
 (846, 'Command: npc spawngroup'),
 (847, 'Command: npc despawngroup'),
 (848, 'Command: gobject spawngroup'),
 (849, 'Command: gobject despawngroup'),
-(850, 'Command: list respawns');
+(850, 'Command: list respawns'),
+(851, 'Command: debug dynamicfactor');
 
-DELETE FROM `rbac_linked_permissions` WHERE `id` = 197 AND `linkedId` BETWEEN 842 AND 846;
+DELETE FROM `rbac_linked_permissions` WHERE `id` = 197 AND `linkedId` BETWEEN 845 AND 850;
 INSERT INTO `rbac_linked_permissions` (`id`, `linkedId`) VALUES
 (197, 846),
 (197, 847),
